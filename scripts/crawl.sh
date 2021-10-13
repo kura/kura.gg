55adcf73733efe5842a3f6841aa6ddf66f79c53b
Kura <kura.hax@gmail.com>
2021-10-13 22:08:06 +0100

    Remove crawler from action and re-add the job queue for crawling since it'll only be run locally

--- scripts/crawl.sh
+++ scripts/crawl.sh
@@ -9,6 +9,9 @@ fi
 for page in `find $1 -type f`
 do
     url=`echo "https://kura.gg${page}" | sed -e "s|$1||g"`
-    echo $url
-    curl -s $url -o /dev/null
+    curl -s $url -o /dev/null &
 done
+
+sleep 2
+echo "waiting for crawl jobs to complete"
+wait < <(jobs -p)
