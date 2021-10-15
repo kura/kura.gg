d69b91430cc9cf56dcbc914d1eb0edad0fd39be2
Kura <kura.hax@gmail.com>
2021-10-15 23:22:27 +0100

    Filter out full path in hash files

--- scripts/md5.sh
+++ scripts/md5.sh
@@ -8,5 +8,7 @@ fi
 
 for f in `find $1 -type f | grep -v -e md5 -e sha1`
 do
-  md5sum $f > $f.md5
+  md5sum $f > $f.md5.bak
+  cat $f.md5.bak | sed -r 's/^([a-z0-9]*) .*\/(.*)$/\1 \2/g' > $f.md5
+  rm $f.md5.bak
 done
--- scripts/sha1.sh
+++ scripts/sha1.sh
@@ -8,5 +8,7 @@ fi
 
 for f in `find $1 -type f | grep -v -e md5 -e sha1`
 do
-  sha1sum $f > $f.sha1
+  sha1sum $f > $f.sha1.bak
+  cat $f.sha1.bak | sed -r 's/^([a-z0-9]*) .*\/(.*)$/\1 \2/g' > $f.sha1
+  rm $f.sha1.bak
 done
