62e7e06794f94924528371eabc36791ae43a8a0d
kura <kura@users.noreply.github.com>
2022-08-12 13:31:22 +0000

    deploy: 744c05467927dfcda98437f944c6a3567ac8fba9

---
+++ theme/js/disqus.js
@@ -0,0 +1,13 @@
+var disqus_config = function () {
+  this.page.url = "https://kura.gg" + window.location.pathname;
+  this.page.identifier = window.location.pathname;
+};
+
+if (document.getElementById('disqus_thread')) {
+  (function() {
+    var d = document, s = d.createElement('script');
+        s.src = '//syslogtv.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+  })();
+}
