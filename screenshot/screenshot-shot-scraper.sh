744c05467927dfcda98437f944c6a3567ac8fba9
Kura <kura.hax@gmail.com>
2022-08-12 14:23:33 +0100

    Try running local dev server for screenshots

--- screenshot/screenshot-shot-scraper.sh
+++ screenshot/screenshot-shot-scraper.sh
@@ -19,7 +19,7 @@ screenshot() {
 for page in `find $1 -name "*.html" | grep -v '/tor/' | grep -v '/donate/' | grep -v '/apt.kura.io/' | grep -v '/curriculum-vitae/' | grep -v '/category/' | grep -v '/categories/' | grep -v '/tag/' | grep -v '/tags/' | grep -v '/authors/' | grep -v '/archives/' | grep -v '/500.html' | grep -v '/404.html'`
 do
     output=`echo $page | sed -e 's/\.html/\.png/g'`
-    url=`echo "https://kura.gg/${page}" | sed -e "s|$1||g"`
+    url=`echo "http://localhost:8000/${page}" | sed -e "s|$1||g"`
     screenshot $url $output
 done
 
