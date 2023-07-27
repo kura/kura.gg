74bf3def277fbf56d7b1f0f0b904ff127cde395b
Kura <kura.hax@gmail.com>
2023-07-27 16:48:13 +0100

    Update CV generator to shrink the PDF file size

--- scripts/update_cv.sh
+++ scripts/update_cv.sh
@@ -6,4 +6,6 @@ cd $REPO
 make build
 cd $CUR
 cp -R $REPO/_site/* cv/
-wkhtmltopdf --enable-local-file-access cv/index.html cv/kura.pdf
+wkhtmltopdf --enable-local-file-access cv/index.html cv/kura_uncomp.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=cv/kura.pdf cv/kura_uncomp.pdf
+rm cv/kura_uncomp.pdf
